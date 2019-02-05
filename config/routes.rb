46f7ace4e26043687ba07a6a46d904b0bf6c9f72
Jorge Navarro <jnavarr56@gmail.com>
2019-02-05 16:25:24 -0500

    removed unesscessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  devise_for :users, controllers: { 
+  devise_for :users, :skip => [:passwords, :registrations], controllers: { 
     
     omniauth_callbacks: 'users/omniauth_callbacks',
     
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
     
     unauthenticated do
       root :to => "sign_in#sign_in"
+
+      get "*path" => "sign_in#sign_in"
     end
 
     authenticated do
